3a23e56dd46cfda8c2c97a47b07d594ccf630846
kimdotcom2 <105586594+kimdotcom2@users.noreply.github.com>
2023-07-23 19:55:58 +0900

    Feat: Add Swagger docs for TourAPI Module

---
+++ src/main/java/com/backend/TravelGuide/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.backend.TravelGuide.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Travel Guide",
+                description = "Travel Guide RestAPI 명세서",
+                version = "v1"))
+
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+
+}
--- src/main/java/com/backend/TravelGuide/tourapi/controller/TourRestAPIController.java
+++ src/main/java/com/backend/TravelGuide/tourapi/controller/TourRestAPIController.java
@@ -2,6 +2,14 @@ package com.backend.TravelGuide.tourapi.controller;
 
 import com.backend.TravelGuide.tourapi.DTO.TourAPIDTO;
 import com.backend.TravelGuide.tourapi.service.TourAPIService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -11,6 +19,7 @@ import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.List;
 
+@Tag(name = "TourAPI 컨트롤러")
 @RestController
 public class TourRestAPIController {
     @Qualifier("tourAPIServiceImpl")
@@ -20,7 +29,21 @@ public class TourRestAPIController {
         this.tourAPIService = tourAPIService;
     }
 
-    @GetMapping("/keywordSearch")
+    @Operation(summary = "키워드 검색", description = "키워드와 페이지 번호를 전달하면 여행지 목록을 JSON으로 반환합니다.")
+    @ApiResponses(
+            {
+                    @ApiResponse(responseCode = "200", description = "OK!", content = @Content(schema = @Schema(implementation = TourAPIDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "500", description = "Server Error")
+            }
+    )
+    @Parameters(
+            {
+            @Parameter(name = "keyword", description = "키워드", example = "타워"),
+            @Parameter(name = "pageNo", description = "페이지 번호", example = "1")
+    }
+    )
+    @GetMapping("/api/tourapi/keywordSearch")
     public List<?> tourListApiByKeyword(
             @RequestParam String keyword,
             @RequestParam String pageNo
@@ -32,7 +55,21 @@ public class TourRestAPIController {
         return tourAPIDTOS;
     }
 
-    @GetMapping("/areaBasedSearch")
+    @Operation(summary = "지역 기반 검색", description = "선택한 지역의 여행지 목록을 가져옵니다.")
+    @ApiResponses(
+            {
+                    @ApiResponse(responseCode = "200", description = "OK!", content = @Content(schema = @Schema(implementation = TourAPIDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "500", description = "Server Error")
+            }
+    )
+    @Parameters(
+            {
+                    @Parameter(name = "areaCode", description = "지역번호 : 1, 2, 31", example = "타워"),
+                    @Parameter(name = "pageNo", description = "페이지 번호", example = "1")
+            }
+    )
+    @GetMapping("/api/tourapi/areaBasedSearch")
     public List<TourAPIDTO> tourListApiByArea(
             @RequestParam String areaCode,
             @RequestParam String pageNo
